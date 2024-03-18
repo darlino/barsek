9774b5b05fd215339ee7c47e1bc9eba2b5b00584
Darlino Noula <darnoul20@gmail.com>
2024-03-18 13:48:11 +0100

    Correct responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex  min-h-screen container  max-w-4xl lg:max-w-6xl 2xl:max-w-7xl flex-col spac-y-2">
       <Navbar />
-      <div className="flex flex-col lg:px-24 -space-y-20 p-8">
+      <div className="flex flex-col lg:px-24 space-y-3 lg:-space-y-20 p-8">
         <div className="space-y-6">
           <h1 className="lg:text-6xl font-bold text-center text-4xl text-slate-80 ">
             Convertissez n'importe quel{" "}
@@ -29,12 +29,12 @@ export default function Home() {
           src="/images/hero.png"
           width={1200}
           height={1200}
-          className="w-full  h-full object-cover mx-auto"
+          className="w-full lg:order-1 order-2 h-full object-cover mx-auto"
           alt=""
         />
         <Link
           href="/dashboard"
-          className="py-3 px-6 mx-auto group transition-all cursor-pointer p-4 text-white flex my-auto rounded-md space-x-3  bg-gradient-to-tr from-primary to-third"
+          className="py-3 justify-center lg:order-2 order-1 px-6 mx-auto group transition-all cursor-pointer p-4 text-white flex my-auto rounded-md space-x-3  bg-gradient-to-tr from-primary to-third"
         >
           <p className=" font-bold">Commencer</p>
           <ArrowRight
--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -10,29 +10,29 @@ type Props = {
 
 const Navbar: FC<Props> = ({ exit = false }) => {
   return (
-    <nav className="flex justify-between w-full py-6 lg:px-24 px-8">
+    <nav className="flex justify-between w-full py-6 lg:px-24 px-1">
       <Image
         alt="logo"
         width={1200}
         height={1200}
-        className="w-24  object-cover"
+        className="lg:w-24 w-16  object-cover"
         src="/images/logo.png"
       />
       {!exit ? (
         <Link
           href="/dashboard"
-          className="py-3 px-6 cursor-pointer p-4 text-white flex my-auto rounded-md space-x-3  bg-gradient-to-tr from-primary to-third"
+          className="py-3 px-4 cursor-pointer p-4 text-white flex my-auto rounded-md space-x-3  bg-gradient-to-tr from-primary to-third"
         >
-          <User size={"20"} />
-          <p className=" font-bold">Dashboard</p>
+          <User size={"16"} />
+          <p className="text-sm font-bold">Dashboard</p>
         </Link>
       ) : (
         <Link
           href="/"
           className="py-3 px-6 cursor-pointer p-4 text-white flex my-auto rounded-md space-x-3   bg-third"
         >
-          <LogOut size={"20"} />
-          <p className=" font-bold">Sortir</p>
+          <LogOut size={"16"} />
+          <p className=" font-bold text-sm">Sortir</p>
         </Link>
       )}
     </nav>
